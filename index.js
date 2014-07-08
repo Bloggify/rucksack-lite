d9106df89ffc4dc61d6fb280e1c9855dcc3a0ea4
Ionică Bizău <bizauionica@yahoo.com>
2014-07-08 20:41:38 +0300

    Pipe file content to response

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 Config.options.clientScripts = [];
-Statique._routes["/_getClientScript"] = function (req, res) {
-    var url = req.url;
-    var fullPath = Config.root + "/plugins/client/"
+
+function serveClientFile(req, res) {
+    var url = req.url
+      , splits = url.split("/")
+      ;
+
+    if (splits.length <= 3) {
+        return Statique.error(req, res, 404);
+    }
+
     debugger;
-    // Fs.createReadStream(fullPath)
-};
+    var plugin = splits[3]
+      , pathToFile = splits.slice(4, splits.length).join("/")
+      ;
+
+    if (!plugin || !pathToFile) {
+        return Statique.error(req, res, 404);
+    }
+
+    var fullPath = Config.root + "/plugins/" + plugin + "/client/" + pathToFile;
+    debugger;
+    Fs.createReadStream(fullPath).pipe(res);
+}
+
+Statique._regexpRoutes.push({
+    regexp: /\/api\/getClientScript\/.*\/.*/
+  , type: "regexp"
+  , url: {
+        get: serveClientFile
+    }
+});
