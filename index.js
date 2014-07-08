3e4c4e8b8f4029b6a3a995caf43c82c753bcc220
Ionică Bizău <bizauionica@yahoo.com>
2014-07-08 20:13:55 +0300

    First concept

---
+++ index.js
@@ -0,0 +1,7 @@
+Config.options.clientScripts = [];
+Statique._routes["/_getClientScript"] = function (req, res) {
+    var url = req.url;
+    var fullPath = Config.root + "/plugins/client/"
+    debugger;
+    // Fs.createReadStream(fullPath)
+};
