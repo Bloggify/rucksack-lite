ddcd9c2b8eb22577b5f93b6ea3ee3da21b727416
Ionică Bizău <bizauionica@yahoo.com>
2014-11-03 18:39:45 +0200

    Fixed file serving

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/**
- * serveClientFile
- * Serves client files from plugins
- *
- * @name serveClientFile
- * @function
- * @param {Object} req The request object
- * @param {Object} res The response object
- * @return
- */
-function serveClientFile(req, res) {
-}
-
 Bloggify.server.page.add(/\/api\/plugin-file\/.*\/.*/, function (lien) {
 
     var splits = lien.pathName.split("/");
@@ -27,6 +14,6 @@ Bloggify.server.page.add(/\/api\/plugin-file\/.*\/.*/, function (lien) {
         return Statique.error(req, res, 404);
     }
 
-    var fullPath = Bloggify.ROOT + "/plugins/" + plugin + "/client/" + pathToFile;
-    lien.file(fullPath);
+    var filePath = "/plugins/" + plugin + "/client/" + pathToFile;
+    lien.file(filePath, Bloggify.ROOT);
 });
