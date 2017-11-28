61501e5b55e3e7b1d4a3573ca9cb3f53f26b7ec1
Ionică Bizău <bizauionica@gmail.com>
2017-11-28 06:45:43 +0200

    First version of rucksack lite

--- example/data/main.js
+++
@@ -1,7 +0,0 @@
-const math = require("./math")
-    , camelo = require("camelo")
-    ;
-
-console.log(math.square(2, 4));
-//console.log($("#my-id"));
-console.log(camelo("hey there"));
--- example/index.js
+++ example/index.js
@@ -3,14 +3,20 @@
 const Rucksack = require("..");
 
 
-let bundler = new Rucksack();
+const r = new Rucksack({
+    jsUrl: "my-bundle.js"
+  , cssUrl: "my-bundle.css"
+});
 
-// Add remote url as resource
-//bundler.add("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js");
-//bundler.add(`${__dirname}/data/main.js`);
-//bundler.add(`${__dirname}/data/another-main.js`);
-bundler.add(`${__dirname}/data/bar.css`);
-bundler.add(`${__dirname}/data/main.css`);
-bundler.addCSS("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css", false);
-bundler.bundleCSS();
-bundler.bundleJS();
+// Local files are supposed to be added via `rucksack`
+// (which implements bundling)
+// Those will *not* appear in the HTML output
+r.add(`${__dirname}/data/bar.css`);
+r.add(`${__dirname}/data/main.css`);
+
+// On the other side, remote scripts and styles *will* appear
+// in the output, because they don't need bundling.
+r.add("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.js");
+r.add("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css", false);
+
+console.log(bundler.html())
